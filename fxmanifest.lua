c3648eea56de5c77ee1f6a320ff2b92be6304082
polisek <107623238+polisek@users.noreply.github.com>
2022-11-19 09:39:45 +0100

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+description 'Polisek scripts'
+
+dependencies {	'ox_lib',}
+shared_scripts {    '@ox_lib/init.lua', }
+client_scripts {'client/client.lua',}
+
+
+lua54 "yes"
